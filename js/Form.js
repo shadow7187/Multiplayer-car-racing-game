8ed9ce1db73699420df1e8439bf73c8a20ac12bf
shadow7187 <67884290+shadow7187@users.noreply.github.com>
2020-10-31 12:55:16 -0500

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,44 @@
+class Form {
+
+  constructor() {
+    this.input = createInput("Name");
+    this.button = createButton('Play!');
+    this.restart = createButton("Restart?")
+    this.greeting = createElement('h2');
+    this.title = createElement('h2');
+  } 
+  hide(){
+    this.title.hide();
+    this.input.hide();
+    this.button.hide();
+    this.greeting.hide();
+  }
+
+  display(){
+    this.title.html("Car Racing Game");
+    this.title.position(displayWidth/2 - 50, 0);
+
+    this.input.position(displayWidth/2 - 40 , displayHeight/2 - 80);
+    this.button.position(displayWidth/2 + 30, displayHeight/2);
+    
+    this.restart.position(displayWidth/2 + 30, displayHeight/1.8);
+    this.restart.hide();
+    this.button.mousePressed(()=>{
+      this.input.hide();
+      this.button.hide();
+      player.name = this.input.value();
+      playerCount+=1;
+      player.index = playerCount;
+      player.update();
+      player.updateCount(playerCount);
+      this.greeting.html("Hello " + player.name)
+      this.greeting.position(displayWidth/2 - 70, displayHeight/4);
+    });
+    this.restart.mousePressed(()=>{
+    player.updateCount(0)
+    game.update(0)
+    });
+
+    
+  }
+}
